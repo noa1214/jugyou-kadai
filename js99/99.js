3268b5e9c936eb3fb5e5cc0badff00e6dccd7203
noa1214 <105455280+noa1214@users.noreply.github.com>
2022-05-27 11:42:39 +0900

    Update 99.js

--- js99/99.js
+++ js99/99.js
@@ -1,6 +1,10 @@
-let html ="";
+let html ="<tr><th></th>";
+for(let x=1; x<=99; x+=1){
+    html += '<th>'+ x + '</th>';
+}
+html += '</tr>';
 for(let y=1; y<=99; y+=1){
-    html += '<tr>';
+    html += '<tr><th>' + y + '</th>';
     for(let x=1; x<=99; x+=1){
         html += '<td>' + x*y + '</td>';
     }
